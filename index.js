89c206ae1c9805bf36f3a303803acdb80e1152ff
leomet07 <leomet07@gmail.com>
2021-05-17 22:02:11 -0400

    Basic functionality.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const low = require("./low");
 (async function () {
 	console.log("Generate a svelte project!");
+	let name = process.argv[2];
+	if (!name) {
+		console.log("No gen name provided.");
+		return;
+	}
+	await low.precheck(name);
+	await low.get_template(name);
+	await low.delete_git(name);
 })();
---
+++ low.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+const { spawn } = require("child_process");
+
+module.exports = {
+	precheck: async function (name) {
+		if (fs.existsSync(name)) {
+			console.log("Deleting the current project under dir: ", name);
+			this.delete_dir(name);
+		}
+	},
+	delete_git: async function (git_path) {
+		this.delete_dir(path.join(git_path, ".git/"));
+	},
+	delete_dir: async function (to_delete) {
+		fs.promises.rmdir(to_delete, { recursive: true });
+	},
+	get_template: async function (name) {
+		const spawned = spawn("git", [
+			"clone",
+			"https://github.com/leomet07/sveltegen.git",
+			name,
+		]);
+
+		return new Promise((resolve) => {
+			spawned.stdout.on("data", (data) => {
+				console.log(data.toString());
+			});
+
+			spawned.stderr.on("data", (data) => {
+				console.error(data.toString());
+			});
+
+			spawned.on("close", () => {
+				resolve();
+			});
+		});
+	},
+};
